64ac58c87f1826a5682244e2324ac03f7356db0e
yostane <1958676+yostane@users.noreply.github.com>
2023-11-08 18:53:46 +0100

    push express server

--- INFO3-2324/first-project/src/main.ts
+++ INFO3-2324/first-project/src/main.ts
@@ -1,13 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+
+const yugiohCards = [
+  {
+    name: "Magicien sombre",
+    attack: 2500,
+  },
+  {
+    name: "Dragon blanc aux yeux bleus",
+    attack: 3000,
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get("/cards", (req, res) => {
+  res.json(yugiohCards);
+});
+
+app.post("/cards", (req, res) => {
+  console.log("adding new card");
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
