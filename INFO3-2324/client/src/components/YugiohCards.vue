c70b04b105466d1733a2eb65c538ba5c630c4f67
yostane <1958676+yostane@users.noreply.github.com>
2023-11-22 18:55:07 +0100

    Add post

--- INFO3-2324/client/src/components/YugiohCards.vue
+++ INFO3-2324/client/src/components/YugiohCards.vue
@@ -4,16 +4,33 @@ import { ref } from 'vue'
 const cards = ref([])
 
 async function loadCards() {
-  const response = await fetch('/cards')
+  const response = await fetch('/api/cards')
   const responseCards = await response.json()
   cards.value = responseCards
 }
 
 loadCards()
+
+async function addCard() {
+  const card = {
+    name: 'Exodia Tête',
+    attack: 500,
+    type: 'attack'
+  }
+  const response = await fetch('/api/cards', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(card)
+  })
+  await loadCards()
+}
 </script>
 
 <template>
   <h2>Cards</h2>
+  <button @click="addCard()">Add Card</button>
   <ul>
     <li v-for="(card, index) in cards" :key="index">Elemnt actuel: {{ card }}</li>
   </ul>
