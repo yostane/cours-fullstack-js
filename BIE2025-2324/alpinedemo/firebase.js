1721cae32adbb1591996ad1d075e800fd11693dd
yostane <1958676+yostane@users.noreply.github.com>
2023-11-22 11:52:44 +0100

    Starting to add firestore

--- BIE2025-2324/alpinedemo/firebase.js
+++ BIE2025-2324/alpinedemo/firebase.js
@@ -1,12 +1,16 @@
 // Import the functions you need from the SDKs you need
+// CDN imports https://firebase.google.com/docs/web/alt-setup?hl=fr
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import { firebaseConfig } from "./firebase-config.js";
+/* create a firebase-config.js in the same folder with this content (replace with your app information)
+export const firebaseConfig = {
   apiKey: "",
   authDomain: "",
   projectId: "",
@@ -15,7 +19,24 @@ const firebaseConfig = {
   appId: "",
   measurementId: "",
 };
+*/
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+window.authenticateUserWithFirebase = async function (email, password) {
+  const auth = getAuth();
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return userCredential.user;
+};
+
+window.addItem = async function addItem(email, item) {};
