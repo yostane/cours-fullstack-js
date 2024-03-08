8cef7482abb67e8077ec246b45e144603b164d71
yostane <1958676+yostane@users.noreply.github.com>
2024-03-08 09:12:59 +0100

    use bun and te jest config

---
+++ projects/2023-2024/api-rest-maitrise/jest.config.ts
@@ -0,0 +1,9 @@
+// https://jestjs.io/docs/configuration
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+
+export default config;
