615195e8fff7dd6e8602f45391292fde78c2e67c
yostane <1958676+yostane@users.noreply.github.com>
2024-03-12 18:40:31 +0100

    rename uploaded file

--- projects/2023-2024/api-rest-maitrise/src/routes/FileRouter.ts
+++ projects/2023-2024/api-rest-maitrise/src/routes/FileRouter.ts
@@ -1,11 +1,23 @@
+import { randomInt } from "crypto";
 import express from "express";
-import { FolderController } from "../controller/FolderController";
 import multer from "multer";
+import fs from "fs/promises";
 
 const upload = multer({ dest: "uploads/" });
 export const fileRouter = express.Router();
 
 fileRouter.post("/upload", upload.single("datafile"), async (req, res) => {
   console.log(req.body, req.body.extra);
-  console.log(req.file?.filename);
+  const file = req.file;
+  if (!file) {
+    res.sendStatus(400);
+    return;
+  }
+  console.log(file);
+  const userId = randomInt(10000000); // a remplacer par l'ID du user courant
+  const fileId = randomInt(10000000); // a remplacer par l'ID du fichier généré par la BDD
+  await fs.rename(
+    file.path,
+    `uploads/${userId}-${fileId}-${file.originalname}`
+  );
 });
