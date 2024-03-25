18bd41a2488bd268d3c7be5ed9d4daecc2a09327
yostane <1958676+yostane@users.noreply.github.com>
2024-03-25 16:19:17 +0100

    Ajout treshold prop

--- projects/2023-2024/reactjs/my-react-app/src/App.jsx
+++ projects/2023-2024/reactjs/my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-import Counter from './components/Counter'
-import Hello from './components/Hello'
-import Header from './components/Header'
-import { useState } from 'react'
-import StreetFigherList from './components/StreetFighterList'
+import "./App.css";
+import Counter from "./components/Counter";
+import Hello from "./components/Hello";
+import Header from "./components/Header";
+import { useState } from "react";
+import StreetFigherList from "./components/StreetFighterList";
 
 function App() {
   const [increment, setIncrement] = useState(1);
@@ -11,15 +11,17 @@ function App() {
     <>
       <Header />
       <Hello />
-      <input value={increment} 
-      onChange={e => setIncrement(+e.target.value)} 
-      required />
+      <input
+        value={increment}
+        onChange={(e) => setIncrement(+e.target.value)}
+        required
+      />
       <Counter initialValue={10} increment={increment} />
       <Counter initialValue={-30} increment={1} />
       <h2>Boucle for</h2>
-      <StreetFigherList />
+      <StreetFigherList treshold={increment} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- projects/2023-2024/reactjs/my-react-app/src/components/StreetFighterList.jsx
+++ projects/2023-2024/reactjs/my-react-app/src/components/StreetFighterList.jsx
@@ -1,28 +1,41 @@
-function StreetFigherList() {
-  const streetFighters = [{
+import PropTypes from "prop-types";
+
+// treshold -> seuil en anglais
+function StreetFigherList({ treshold }) {
+  const streetFighters = [
+    {
       name: "ryu",
       specialArrack: "Hadoken",
-      hp: 100
-    },{
-      "name": "Sagat",
-      "specialArrack": "Tiger Uppercut",
-      hp: 110
-    }
+      hp: 30,
+    },
+    {
+      name: "Sagat",
+      specialArrack: "Tiger Uppercut",
+      hp: 110,
+    },
   ];
 
-  const streetFighterItems = streetFighters.map(streetFighter => 
+  const streetFighterItems = streetFighters.map((streetFighter) => (
     <li key={streetFighter.name}>
-      Name: {streetFighter.name}. 
+      Name: {streetFighter.name}.
       <ol>
         <li>Power: {streetFighter.specialArrack}</li>
-        <li>HP: {streetFighter.hp}</li>
+        <li
+          style={{
+            color: streetFighter.hp < treshold ? "red" : "green",
+          }}
+        >
+          HP: {streetFighter.hp}
+        </li>
       </ol>
     </li>
-    )
+  ));
 
-  return (
-    <ul>{streetFighterItems}</ul>
-  )
+  return <ul>{streetFighterItems}</ul>;
 }
 
-export default StreetFigherList
+StreetFigherList.propTypes = {
+  treshold: PropTypes.number.isRequired,
+};
+
+export default StreetFigherList;
