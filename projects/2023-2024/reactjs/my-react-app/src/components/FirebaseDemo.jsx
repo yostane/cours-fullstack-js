08d4ee8a63572ea5a92c647b8e61bba87a2546ba
yostane <1958676+yostane@users.noreply.github.com>
2024-03-25 18:02:04 +0100

    login et pwd via des inputs

--- projects/2023-2024/reactjs/my-react-app/src/components/FirebaseDemo.jsx
+++ projects/2023-2024/reactjs/my-react-app/src/components/FirebaseDemo.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export default function FirebaseDemo() {
-  function createUser() {}
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("password");
+  async function createUser() {
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredentials.user.email, userCredentials.user.uid);
+  }
 
   return (
     <div>
       <h1>Firebase Demo</h1>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          name="email"
+          required
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          name="password"
+          required
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
       <button onClick={createUser}>Create user</button>
     </div>
   );
