91592cf9fdbdfe7f629cf207d0d50fd5e8fcb5e5
yostane <1958676+yostane@users.noreply.github.com>
2024-03-26 18:17:19 +0100

    fighter null

--- projects/2023-2024/reactjs/my-react-app/src/pages/FighterDetailPage.jsx
+++ projects/2023-2024/reactjs/my-react-app/src/pages/FighterDetailPage.jsx
@@ -2,9 +2,19 @@ import { useParams } from "react-router-dom";
 
 export default function FighterDetailPage() {
   const params = useParams();
+  const fighter = null;
   return (
     <>
       <h1>Fighter Details {params.name}</h1>
+      {fighter ? (
+        <>
+          <p>{fighter.name}</p>
+          <p>{fighter.hp}</p>
+          <p>{fighter.specialAttack}</p>
+        </>
+      ) : (
+        <p style={{ color: "red" }}>No fighter found</p>
+      )}
     </>
   );
 }
