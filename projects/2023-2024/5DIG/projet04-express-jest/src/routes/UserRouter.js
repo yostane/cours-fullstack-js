432f2032f2a9c28b0f7038c777a9433cb81dddd9
yostane <1958676+yostane@users.noreply.github.com>
2024-04-05 18:45:28 +0200

    implement login

--- projects/2023-2024/5DIG/projet04-express-jest/src/routes/UserRouter.js
+++ projects/2023-2024/5DIG/projet04-express-jest/src/routes/UserRouter.js
@@ -5,25 +5,34 @@ import { JWT_SECRET } from "../constants.js";
 
 export const userRouter = express.Router();
 
+function generateOkBody(user) {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      isVet: user.isVet ?? false,
+      year: 2024,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+  return { token };
+}
+
 userRouter.post("/login", (req, res) => {
-  res.end();
+  const userService = new UserService();
+  const user = userService.find(req.body.email, req.body.password);
+  if (!user) {
+    res.status(401).end();
+  } else {
+    res.json(generateOkBody());
+  }
 });
 
 userRouter.post("/register", (req, res) => {
   const userService = new UserService();
   userService.add(req.body);
-  res.json({
-    toekn: jwt.sign(
-      {
-        id: req.body.id,
-        email: req.body.email,
-        isVet: req.body.isVet ?? false,
-        year: 2024,
-      },
-      JWT_SECRET,
-      {
-        expiresIn: "1d",
-      }
-    ),
-  });
+  res.json(generateOkBody());
 });
