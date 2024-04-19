c43b0bde8033a8bb683c36afec86f6adb32199e4
yostane <1958676+yostane@users.noreply.github.com>
2024-04-19 18:57:05 +0200

    Ajout classe apointment

--- projects/2023-2024/5DIG/projet05-express-sequelize/src/app.js
+++ projects/2023-2024/5DIG/projet05-express-sequelize/src/app.js
@@ -8,8 +8,14 @@ import "./service/authentication.js";
 import { sequelize } from "./service/database.js";
 import { User } from "./model/User.js";
 import { Animal } from "./model/Animal.js";
+import { Apointment } from "./model/Apointment.js";
 User.hasMany(Animal);
 Animal.belongsTo(User);
+
+Apointment.hasOne(User);
+Apointment.hasOne(Animal);
+Animal.hasMany(Apointment);
+User.hasMany(Apointment);
 sequelize.sync();
 
 export const app = express();
