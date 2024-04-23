4697abae1c17bd145e273a00d19f3abfb1ac47ea
yostane <1958676+yostane@users.noreply.github.com>
2024-04-23 12:51:37 +0200

    Better login component

--- projects/2023-2024/reactjs/my-react-app/src/components/FirebaseAuthDemo.jsx
+++ projects/2023-2024/reactjs/my-react-app/src/components/FirebaseAuthDemo.jsx
@@ -9,13 +9,14 @@ import {
 export default function FirebaseAuthDemo() {
   const [email, setEmail] = useState("test@test.com");
   const [password, setPassword] = useState("password");
+  const [currentUser, setCurrentUser] = useState();
   async function createUser() {
     const userCredentials = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-    console.log(userCredentials.user.email, userCredentials.user.uid);
+    setCurrentUser(userCredentials);
   }
 
   async function login() {
@@ -24,40 +25,49 @@ export default function FirebaseAuthDemo() {
       email,
       password
     );
-    console.log(userCredentials.user.email, userCredentials.user.uid);
+    setCurrentUser(userCredentials);
   }
 
   async function logout() {
     await signOut(auth);
     console.log("Logged out");
+    setCurrentUser(null);
   }
 
   return (
     <div>
       <h1>Firebase Demo</h1>
-      <div>
-        <label htmlFor="email">Email: </label>
-        <input
-          name="email"
-          required
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input
-          name="password"
-          required
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={createUser}>Create user</button>
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
+      {currentUser ? (
+        <>
+          <p>Hello {currentUser.user.email}</p>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              name="email"
+              required
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              name="password"
+              required
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button onClick={createUser}>Create user</button>
+          <button onClick={login}>Login</button>
+        </>
+      )}
     </div>
   );
 }
