732f3ac67231ab1bb5fc95c7ef740d32423fd8fe
yostane <1958676+yostane@users.noreply.github.com>
2024-07-01 10:20:27 +0200

    feat: Add HTTP calls for creating and deleting files

--- projects/2023-2024/3AL-2324/02-file-database/src/routes/FileRouter.ts
+++ projects/2023-2024/3AL-2324/02-file-database/src/routes/FileRouter.ts
@@ -1,7 +1,9 @@
+import { log } from "console";
 import express from "express";
 
 const files = [
   {
+    id: 1,
     name: "image01.png",
     size: 200,
     url: "https://placehold.co/600x400",
@@ -15,13 +17,25 @@ fileRouter.get("/", (req, res) => {
 });
 
 fileRouter.post("/", (req, res) => {
+  const file = req.body;
+  // Un auto increment fait à la main
+  file.id = files[files.length - 1].id + 1;
+  files.push(file);
+  res.sendStatus(201);
+});
+
+fileRouter.put("/:id", (req, res) => {
+  const newFile = req.body;
   res.end();
 });
 
-fileRouter.put("/", (req, res) => {
-  res.end();
-});
-
-fileRouter.delete("/", (req, res) => {
+// :id est un path param
+// ?p1=v1&p2=v2 sont des query params
+fileRouter.delete("/:id", (req, res) => {
+  const id = +req.params.id;
+  console.log("deleting file with id from path parameter", id);
+  // méthode 1
+  const index = files.findIndex((file) => file.id === id);
+  files.splice(index, 1);
   res.end();
 });
--- projects/2023-2024/3AL-2324/02-file-database/src/server.ts
+++ projects/2023-2024/3AL-2324/02-file-database/src/server.ts
@@ -2,9 +2,16 @@
 // On ne me pas les accolades si on veut récupérer l'export par défaut
 import express from "express";
 import { fileRouter } from "./routes/FileRouter";
+import bodyParser from "body-parser";
+import { myLogger, myOtherLogger } from "./middlewares/loggerMiddleware";
 
 // usine (factory) qui crée un serveur express
 // On exporte le serveur pour pouvoir l'utiliser dans main.ts
 export const server = express();
 
+// server.use(myLogger);
+// middleware qui traite le json
+server.use(bodyParser.json());
+// server.use(myOtherLogger);
+// middleware qui traite les urls en sur le chemin racine (/)
 server.use("/", fileRouter);
