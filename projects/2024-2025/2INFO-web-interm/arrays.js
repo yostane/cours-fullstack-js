14cc8f2ba3cad052ff46c5ed2b648b66d555551d
yostane <1958676+yostane@users.noreply.github.com>
2024-10-14 16:49:34 +0200

    Refactor array manipulation code to use declarative style

--- projects/2024-2025/2INFO-web-interm/arrays.js
+++ projects/2024-2025/2INFO-web-interm/arrays.js
@@ -9,6 +9,7 @@ for (const number of numbers) {
 }
 
 // multiplier les éléments pairs par deux
+// style impératif
 const result = [];
 for (const number of numbers) {
   if (number % 2 === 0) {
@@ -16,3 +17,9 @@ for (const number of numbers) {
   }
 }
 console.log(result);
+
+// style déclaratif
+const filteredNumbers = numbers.filter((number) => number % 2 === 0);
+const transformedNumbers = filteredNumbers.map((number) => number * 2);
+console.log(filteredNumbers, transformedNumbers);
+console.log(numbers.filter((n) => n % 2 === 0).map((n) => n * 2));
