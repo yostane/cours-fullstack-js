b66d9fdda8f524981684d7a030b227d5ae810341
yostane <1958676+yostane@users.noreply.github.com>
2024-10-14 16:19:20 +0200

    Add object literals with Saiyajin characters and their properties

---
+++ projects/2024-2025/2INFO-web-interm/objects.js
@@ -0,0 +1,23 @@
+// Objets littéraux
+const saiyajin1 = {
+  name: "Sans le cou",
+  power: 2000,
+
+  launchAttack: function () {
+    console.log("Kamehameha");
+  },
+};
+
+const saiyajin2 = {
+  name: "Végétal",
+  power: 1900,
+};
+
+const saiyajin3 = {
+  name: "Truc muche",
+  power: 1500,
+  age: 8,
+};
+
+console.log(saiyajin1.name);
+saiyajin1.launchAttack();
