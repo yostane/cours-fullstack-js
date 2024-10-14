7b653e16a207784549acd54b7fc64a62331673d3
yostane <1958676+yostane@users.noreply.github.com>
2024-10-14 15:30:50 +0200

    Refactor hello.js file to add additional functions and console logs

--- projects/2024-2025/2INFO-web-interm/hello.js
+++ projects/2024-2025/2INFO-web-interm/hello.js
@@ -6,9 +6,38 @@ console.log("x", x);
 // consntante, variable non réassignable
 const y = 20;
 
-function add(x, y) {
+function add(x = "Hello", y = 0) {
   return x + y;
 }
 
 console.log(add(30, 44));
 console.log(add("Hello", -2));
+console.log(add());
+console.log(add(10));
+
+function printMessage(message) {
+  console.log(message);
+}
+
+printMessage("Hello");
+printMessage();
+
+console.log(5 / 0);
+console.log(5 + undefined);
+console.log('+"123" + 6 ->', +"123" + 6);
+console.log('+"a" ->', +"a");
+console.log("B" + "a" + +"a" + "a");
+
+const f = add;
+console.log(f(3, 5));
+
+function printCalculation(f) {
+  const result = f(4, 5);
+  console.log(result);
+}
+
+printCalculation(add);
+printCalculation(function (x, y) {
+  return x - y;
+});
+printCalculation((x, y) => x - y);
