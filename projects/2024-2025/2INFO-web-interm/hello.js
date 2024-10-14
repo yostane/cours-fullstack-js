4aac41d6fbe1f4c0cb31dd050c1e74cf26067e57
yostane <1958676+yostane@users.noreply.github.com>
2024-10-14 15:17:23 +0200

    Add hello.js file with console logs and add function

---
+++ projects/2024-2025/2INFO-web-interm/hello.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+let x = 10;
+console.log("x", x);
+x = "Hello";
+console.log("x", x);
+// consntante, variable non réassignable
+const y = 20;
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(30, 44));
+console.log(add("Hello", -2));
