f7e24d40ffeaf401e7eefa872ffc5b75f0a0b6fd
yostane <1958676+yostane@users.noreply.github.com>
2024-10-14 17:09:23 +0200

    Refactor array manipulation code to use map and ternary operator

---
+++ projects/2024-2025/2INFO-web-interm/corrige-exos.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {number[]} numbers
+ * @returns {number[]}
+ */
+function exo1(numbers) {
+  return numbers.map((n) => (n % 2 === 0 ? n * 3 : n * 2));
+}
+
+console.log(exo1([1, 2, 3, 4, 5]));
+console.log(exo1([11, -2, 76, 91, 5]));
