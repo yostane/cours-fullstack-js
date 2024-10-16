341aeac21f248a9e752b10f12efe7435605a0d5a
yostane <1958676+yostane@users.noreply.github.com>
2024-10-16 16:33:50 +0200

    Refactor StateDemo component to include message state and display its length

--- projects/2024-2025/2INFO-web-interm/my-vue-app/src/components/StateDemo.jsx
+++ projects/2024-2025/2INFO-web-interm/my-vue-app/src/components/StateDemo.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 
 export default function StateDemo() {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Hello");
 
   return (
     <>
       <div>
         The current count is: {count}
         <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(newText) => setMessage(newText)}
+        />
+        The current message is: {message}. Its length is {message.length}
       </div>
     </>
   );
