8f4587af7404ee6e31ac4209439cdb9406528d0c
yostane <1958676+yostane@users.noreply.github.com>
2024-10-16 16:09:22 +0200

    Refactor MessageShow component to display the length of the message

--- projects/2024-2025/2INFO-web-interm/my-vue-app/src/components/MessageShow.jsx
+++ projects/2024-2025/2INFO-web-interm/my-vue-app/src/components/MessageShow.jsx
@@ -1,7 +1,15 @@
+import PropTypes from "prop-types";
+
 export default function MessageShow({ message }) {
   return (
     <>
-      <p>The message is: {message}</p>
+      <p>
+        The message is: {message}. Its length is: {message.length}
+      </p>
     </>
   );
 }
+
+MessageShow.propTypes = {
+  message: PropTypes.string.isRequired,
+};
