710448cf33fb5c27c4dcdd846900487054dd297d
yostane <1958676+yostane@users.noreply.github.com>
2024-10-28 08:39:34 +0100

    Refactor .gitignore and .vscode/settings.json, and add ExoCounter component
    - Refactor .gitignore to include venv/ and .vscode/ directories
    - Update .vscode/settings.json to add spellright language and document types
    - Add ExoCounter component to App.jsx for exercises

--- projects/2024-2025/2INFO-web-interm/my-vue-app/src/App.jsx
+++ projects/2024-2025/2INFO-web-interm/my-vue-app/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import ExoCount from "./components/ExoCounter";
 import Hello from "./components/Hello";
 import MessageShow from "./components/MessageShow";
 import StateDemo from "./components/StateDemo";
@@ -12,6 +13,11 @@ function App() {
       <MessageShow message="I 💖 react" />
       <StateDemo />
       <StateDemo />
+      <h1>Exercices</h1>
+      <li>Exerice 1: <ExoCount /></li>
+      <li>Exerice 2</li>
+      <li>Exerice 3</li>
+      <li>Exerice 4</li>
     </>
   );
 }
---
+++ projects/2024-2025/2INFO-web-interm/my-vue-app/src/components/ExoCounter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function ExoCount(){
+  const initialValue = 42;
+  // const {état, fonction de maj de l'état} = useState(val par défaut);
+  const {count, setCount} = useState(initialValue);
+
+  return <>
+  <button onClick={() => setCount(count + 1)}>+</button>
+  <button onClick={() => setCount(count - 1)}>-</button>
+  <button onClick={() => setCount(initialValue)}>Reset</button>
+  </>;
+}
