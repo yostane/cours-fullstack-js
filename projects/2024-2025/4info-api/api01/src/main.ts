9e9fdb67b98c409dccae4985ad7e0d5c42705533
yostane <1958676+yostane@users.noreply.github.com>
2024-11-04 11:47:13 +0100

    Add drive-simu endpoint and implement drive file listing

--- projects/2024-2025/4info-api/api01/src/main.ts
+++ projects/2024-2025/4info-api/api01/src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 
@@ -6,12 +7,19 @@ app.get("/", (req, res) => {
   res.send(`<h1>Holà ${new Date()}</h1>`);
 });
 
-app.get("/drive", (req, res) => {
+app.get("/drive-simu", (req, res) => {
   res.json([
     { name: "anime.txt", isFile: true },
     { name: "courses.txt", isFile: true },
     { name: "souvenirs", isFile: false },
   ]);
+});
+
+app.get("/drive", async (req, res) => {
+  const dir = await fs.opendir("./drive");
+  for await (const file of dir) {
+    console.log(file.name, file.isDirectory());
+  }
 });
 
 app.listen(3000, () => {
