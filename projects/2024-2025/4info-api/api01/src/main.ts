2bdce7c080ee0b6db2450bfafb84c3b290df2042
yostane <1958676+yostane@users.noreply.github.com>
2024-11-04 10:53:36 +0100

    Add initial setup for API with nodemon configuration and update main route

--- projects/2024-2025/4info-api/api01/src/main.ts
+++ projects/2024-2025/4info-api/api01/src/main.ts
@@ -2,8 +2,9 @@ import express from "express";
 
 const app = express();
 
+
 app.get("/", (req, res) =>{
-  res.send("<h1></h1>");
+  res.send(`<h1>Hello ${new Date()}</h1>`);
 });
 
 app.listen(3000, () => {
