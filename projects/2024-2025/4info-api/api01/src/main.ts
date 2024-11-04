c5c33d7efc75287531a7579887173ba8f91cefa1
yostane <1958676+yostane@users.noreply.github.com>
2024-11-04 10:19:12 +0100

    Set up Express server and update package.json for module support

--- projects/2024-2025/4info-api/api01/src/main.ts
+++ projects/2024-2025/4info-api/api01/src/main.ts
@@ -1,1 +1,11 @@
-console.log("I love TypeScript");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) =>{
+  res.send("<h1></h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Serveur démarré");
+});
