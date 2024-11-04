bd3104377c6b2c71f33d0a2b1e668aa4f661d435
yostane <1958676+yostane@users.noreply.github.com>
2024-11-04 11:17:27 +0100

    Add anime and courses text files, update main route for drive endpoint

--- projects/2024-2025/4info-api/api01/src/main.ts
+++ projects/2024-2025/4info-api/api01/src/main.ts
@@ -2,9 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Holà ${new Date()}</h1>`);
+});
 
-app.get("/", (req, res) =>{
-  res.send(`<h1>Hello ${new Date()}</h1>`);
+app.get("/drive", (req, res) => {
+  res.json([
+    { name: "anime.txt", isFile: true },
+    { name: "courses.txt", isFile: true },
+    { name: "souvenirs", isFile: false },
+  ]);
 });
 
 app.listen(3000, () => {
