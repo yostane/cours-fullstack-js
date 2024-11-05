a15d2c79ad33bbc902b96303de93fb39e26524e3
yostane <1958676+yostane@users.noreply.github.com>
2024-11-05 10:17:02 +0100

    Add basic authorization middleware for /drive endpoint

--- projects/2024-2025/4info-api/api01/src/main.ts
+++ projects/2024-2025/4info-api/api01/src/main.ts
@@ -10,7 +10,23 @@ app.get("/", (req, res) => {
   res.send(`<h1>Holà ${new Date()}</h1>`);
 });
 
-app.use("/drive", driveRouter);
+app.use(
+  "/drive",
+  (req, res, next) => {
+    // Authorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l
+    // la partie parès basic: base64(login:password)
+    const authorization = req.headers.authorization;
+    if (authorization == undefined || !authorization.startsWith("Basic ")) {
+      res.status(401).send();
+      return;
+    }
+    const b64Credentials = authorization.substring(6);
+    const credentials = atob(b64Credentials);
+    console.log(b64Credentials, credentials);
+    res.send();
+  },
+  driveRouter
+);
 
 app.listen(3000, () => {
   console.log("Serveur démarré");
