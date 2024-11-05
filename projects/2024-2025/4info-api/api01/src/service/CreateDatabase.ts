3f88936a035fd6fd5f32f1796cad0a8377356c66
yostane <1958676+yostane@users.noreply.github.com>
2024-11-05 09:23:53 +0100

    Update .gitignore and package.json; add CreateDatabase script for SQLite setup

---
+++ projects/2024-2025/4info-api/api01/src/service/CreateDatabase.ts
@@ -0,0 +1,12 @@
+import { Database } from "bun:sqlite";
+import fs from "fs/promises";
+
+await fs.rm("./db.sqlite3");
+const db = new Database("./db.sqlite3", { create: true });
+db.query(`CREATE TABLE USERS(login TEXT, password TEXT)`).run();
+db.query(
+  `INSERT INTO USERS VALUES ('luffy', '${await Bun.password.hash("niku")}')`
+).run();
+db.query(
+  `INSERT INTO USERS VALUES ('naruto', '${await Bun.password.hash("ramen")}')`
+).run();
