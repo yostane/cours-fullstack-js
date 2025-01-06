21bee75b2258a5b4780d531095948fa98b77b8cb
yostane <1958676+yostane@users.noreply.github.com>
2025-01-06 10:49:10 +0100

    Update package.json and add chat-gtp.ts: change chatGTP script to point to new file and implement Google Generative AI model

---
+++ material/ai/langchainjs_bun_demo/src/chat-gtp.ts
@@ -0,0 +1,18 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { marked } from "marked";
+import TerminalRenderer from "marked-terminal";
+
+marked.setOptions({
+  renderer: new TerminalRenderer(),
+});
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-1.5-flash-8b",
+  maxOutputTokens: 2048,
+});
+
+const prompt = Bun.argv[2];
+console.log(marked(`**Human**: ${prompt}`));
+
+const res = await model.invoke([["human", prompt]]);
+console.log(marked(`**AI**: ${res.content.toString().trim()}`));
